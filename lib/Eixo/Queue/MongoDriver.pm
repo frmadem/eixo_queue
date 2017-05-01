625013d4a068d8f69c51b37603e7cf39651d5e67
Francisco Maseda Muiño <frmadem@gmail.com>
2017-05-01 02:31:01 +0200

    Support for JobCron and use of differente collections and classes

--- lib/Eixo/Queue/MongoDriver.pm
+++ lib/Eixo/Queue/MongoDriver.pm
@@ -19,7 +19,7 @@ has(
 );
 
 sub addJob{
-	my ($self, $job) = @_;
+	my ($self, $job, $collection) = @_;
 
 	unless($job->isa('Eixo::Queue::Job')){
 	
@@ -27,7 +27,7 @@ sub addJob{
 
 	}
 
-	$self->getCollection->insert_one({
+	$self->getCollection($collection)->insert_one({
         _id => $job->id,
         %{$job->to_hash}
     });
@@ -35,9 +35,10 @@ sub addJob{
 }
 
 sub updateJob{
-	my ($self, $job) = @_;
+	my ($self, $job, $collection) = @_;
 
-	$self->getCollection->update(
+	$self->getCollection($collection)->update(
+
         {_id=>$job->id} ,
 
         $job->to_hash
@@ -45,15 +46,21 @@ sub updateJob{
 }
 
 sub getJob{
-	my ($self, $id) = @_;
+	my ($self, $id, $collection, $class) = @_;
 
-	$self->__format(
+    my $method = ($class) ? "__formatClass" : "__format";
 
-		$self->getCollection->find({
+	$self->$method(
+
+		[
+            $self->getCollection($collection)->find({
 		
-			_id=>$id
+			    _id=>$id
 
-		})->next
+		    })->next
+        ],
+
+        $class
 
 	);
 }
@@ -63,13 +70,15 @@ sub find{
 
     $self->__format(
 
-        $self->getCollection
+        [
+            $self->getCollection
         
-            ->find($query)
+                ->find($query)
      
-            ->sort($sort)
+                ->sort($sort)
 
-            ->all
+                ->all
+        ]
     );
 }
 
@@ -85,19 +94,21 @@ sub getPendingJob{
 
 	$self->__format(
 
-		$self->getCollection->find_and_modify({
+		[
+            $self->getCollection->find_and_modify({
 
-			query =>$query,
-			sort => {creation_timestamp => 1},
-			update => {
-				'$set' => {
-					status => 'PROCESSING', 
-					start_timestamp => time
-				}
-			},
-			new => 1,
-		})
+			    query =>$query,
+			    sort => {creation_timestamp => 1},
+			    update => {
+				    '$set' => {
+					    status => 'PROCESSING', 
+					    start_timestamp => time
+				    }
+			    },
+			    new => 1,
+		    })
 
+        ]
 	);
 
 }
@@ -113,9 +124,11 @@ sub getJobsCron{
         
     };
 
-    $query->{queue} = $args{queue} if(defined( $args{queue});
+    $query->{queue} = $args{queue} if(defined( $args{queue}));
 
-    my @jobs = $self->getCollection
+    my @jobs = $self->getCollection(
+        $args{collection_cron}
+    )
 
          ->find($query)
 
@@ -131,13 +144,25 @@ sub getJobsCron{
 }
 
 sub __format{
-	my ($self, @jobs) = @_;
+	my ($self, $jobs) = @_;
 
-	@jobs = map {
+	my @jobs = map {
 
 		Eixo::Queue::Job->new(%$_);
 
-	} grep { ref($_) } @jobs;
+	} grep { ref($_) } @$jobs;
+
+	wantarray ? @jobs : (@jobs < 2) ? $jobs[0] : \@jobs;
+}
+
+sub __formatClass{
+    my ($self, $jobs, $class) = @_;
+
+	my @jobs = map {
+
+		$class->new(%$_);
+
+	} grep { ref($_) } @$jobs;
 
 	wantarray ? @jobs : (@jobs < 2) ? $jobs[0] : \@jobs;
 }
